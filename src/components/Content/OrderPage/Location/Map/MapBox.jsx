524b19271efa3a9ac280263d2564e2cad03696c8
VolkovSA <stas_volkov_2017@mail.ru>
2021-08-25 21:31:11 +0300

    Сделано увеличение zoom (в зависимоости от  заполнения input1) с помощью React.useEffect

--- src/components/Content/OrderPage/Location/Map/MapBox.jsx
+++ src/components/Content/OrderPage/Location/Map/MapBox.jsx
@@ -4,24 +4,34 @@ import { YMaps, Map, Clusterer, Placemark } from "react-yandex-maps";
 import { points, gradientColors } from "./data";
 
 const MapBox = (props) => {
-    function checkvalue(params) {
-        let city = params.city;
-        if ((city === null) || (city === '') || (city === undefined)) {
-            return 0;
-        }
-        else return 1;
-    }
     const [zoom, setZoom] = React.useState(9)
-    const mapState = { center: [55.75, 37.57], zoom }
-    
+
+    React.useEffect(() => {
+        let city = props.city;
+        if ((city === null) || (city === '') || (city === undefined)) {
+            return setZoom(1);
+        }
+        return setZoom(9);
+    }, [props])
+    // function checkvalue(params) {
+    //     let city = params.city;
+    //     if ((city === null) || (city === '') || (city === undefined)) {
+    //         return 0;
+    //     }
+    //     else return 1;
+    // }
+    // const [zoom, setZoom] = React.useState(9)
+     const mapState = { center: [55.75, 37.57], zoom }
+
     //!!!!!!!!!!!!!!!!!!!!!!!!!!
     //!Проблемное место
-    let coord = (checkvalue(props) ? setZoom(9) : setZoom(1))
-    
+    // let coord = (checkvalue(props) ? setZoom(9) : setZoom(1))
+
 
     return (
         <div className={ob.mapbox}>
-            {console.log('props:', props, 'checkvalue:', checkvalue(props), 'zoom:', zoom)}
+            {console.log('props:', props, 'checkvalue:', 'zoom:', zoom)}
+            {/* {console.log('props:', props, 'checkvalue:', checkvalue(props), 'zoom:', zoom)} */}
             <div className={ob.map_wrapper1}>
                 <YMaps
                     onApiAvaliable={ymaps => this.geocode(ymaps)}
@@ -48,7 +58,7 @@ const MapBox = (props) => {
                         </Clusterer>
                         <p>
 
-                            
+
                         </p>
                     </Map>
                 </YMaps>
